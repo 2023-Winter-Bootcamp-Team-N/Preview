17e38dd8df5ecaeba28a8304f1cfe11856919650
김주언 <wndjs803@naver.com>
2024-01-03 21:21:13 +0900

    Chore : project setting

--- backend/nTeamProject/settings.py
+++ backend/nTeamProject/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'summary',
+    'account',
 ]
 
 MIDDLEWARE = [
