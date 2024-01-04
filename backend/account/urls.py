368e526c0a5dbaa94d42b828f5dae5459e14660d
jihye9871 <jihye9871@naver.com>
2024-01-04 20:54:24 +0900

    feat: 구독 취소 기능 로직 구현

--- backend/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import SubscribeAPIView, SubscribeCancelAPIView
-
-urlpatterns = [
-    path('subscribe', SubscribeAPIView.as_view()),
-    path('unscribe', SubscribeCancelAPIView.as_view()),
-]
