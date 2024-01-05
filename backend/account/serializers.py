a34f87f4d7924af74b8f8fa71cdc8e683af7cecd
jihye9871 <jihye9871@naver.com>
2024-01-05 17:28:50 +0900

    feat: 요약본 저장 기능 중간 점검

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Subscribe
+from .models import Subscribe, Summary, Category, Summary_By_Time
 
 class SubscribeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,18 @@ class SubscribeCancelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscribe
         fields = ['user_id', 'subscribe_channel']
+
+class SummarySaveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Summary
+        fields= ['user_id', 'youtube_channel', 'youtube_title', 'youtube_url', 'youtube_thumbnail', 'content']
+
+class CategorySaveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['summary_id', 'category']
+
+class SummaryByTimeSaveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Summary_By_Time
+        fields = ['summary_id','start_time', 'end_time', 'content']
--- backend/account/views.py
+++ backend/account/views.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 
-from .models import Subscribe
-from .serializers import SubscribeSerializer, SubscribeCancelSerializer
+from .models import Subscribe, User, Summary, Summary_By_Time, Category
+from .serializers import SubscribeSerializer, SubscribeCancelSerializer 
+from .serializers import SummarySaveSerializer, CategorySaveSerializer, SummaryByTimeSaveSerializer
 
 class SubscribeAPIView(APIView):
     def post(self, request):
@@ -30,5 +31,67 @@ class SubscribeCancelAPIView(APIView):
                 return Response({"error": "구독 정보를 찾지 못했습니다"}, status=status.HTTP_404_NOT_FOUND)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+
+class SummarySaveAPIView(APIView):
+    def post(self, request):
+        # 요약본 저장
+        summary_serializer = SummarySaveSerializer(data=request.data.get('summary'))
+        if summary_serializer.is_valid():
+            summary = summary_serializer.save()
+        else:
+            return Response(summary_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-#deleted_at 과정 만들기 (기존에 잇는걸 찾고 나서 바꿔야함)
+        # 카테고리 저장
+        for category_data in request.data.get('categories', []):
+            category_data['summary_id'] = summary.id
+            category_serializer = CategorySaveSerializer(data=category_data)
+            if category_serializer.is_valid():
+                category_serializer.save()
+            else:
+                return Response(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+
+        # 시간대별 요약
+        for summary_by_time_data in request.data.get('summary_by_times', []):
+            summary_by_time_data['summary_id'] = summary.id
+            summary_by_time_serializer = SummaryByTimeSaveSerializer(data=summary_by_time_data)
+            if summary_by_time_serializer.is_valid():
+                summary_by_time_serializer.save()
+            else:
+                return Response(summary_by_time_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        return Response({"message": "요약본 저장을 성공했습니다."}, status=status.HTTP_201_CREATED)
+        
+# class SummarySaveAPIView(APIView):
+#     def post(self, request):
+#         serializer = SummarySaveSerializer(data=request.data.summaries)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"message": "저장을 성공했습니다."}, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response({"error", "저장에 실패했습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class CategorySaveAPIView(APIView):
+#     def post(self, request):
+#         for category_data in request.data.get('categories', []):
+#             serializer = CategorySaveSerializer(data=category_data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#             else:
+#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response({"message": "카테고리 저장을 완료했습니다."}, status=status.HTTP_201_CREATED)
+        
+
+# class SummaryByTimeAPIView(APIView):
+#     def post(self, request):
+#         for summary_by_time_data in request.data.get('summary_by_times', []):
+#             serializer = SummaryByTimeSaveSerializer(data=summary_by_time_data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#             else:
+#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response({"message": "시간대별 요약에 성공했습니다."}, status=status.HTTP_201_CREATED)
+    
+        
