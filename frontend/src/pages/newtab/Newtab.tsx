4909d4cf01e1c37004825d6f0a6aa7c0cc29b078
HSCEHOL <knr0012@naver.com>
2024-01-08 02:19:58 +0900

    초기 퍼블리싱

--- frontend/src/pages/newtab/Newtab.tsx
+++ frontend/src/pages/newtab/Newtab.tsx
@@ -1,48 +1,77 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React, { useState } from 'react';
 import '@pages/newtab/Newtab.css';
 import '@pages/newtab/Newtab.scss';
 import '../sidepanel/index.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-
-const Newtab = () => {
-  const theme = useStorage(exampleThemeStorage);
-
+import animal from '../../assets/img/animal.svg';
+import art from '../../assets/img/art.svg';
+import cook from '../../assets/img/cook.svg';
+import economy from '../../assets/img/economy.svg';
+import edu from '../../assets/img/edu.svg';
+import enter from '../../assets/img/enter.svg';
+import game from '../../assets/img/game.svg';
+import health from '../../assets/img/health.svg';
+import music from '../../assets/img/music.svg';
+import science from '../../assets/img/science.svg';
+import social from '../../assets/img/social.svg';
+import sport from '../../assets/img/sport.svg';
+import stc from '../../assets/img/stc.svg';
+import travel from '../../assets/img/travel.svg';
+import smile from '../../assets/img/smile.svg';
+import chart from '../../assets/img/chart.svg';
+import TeamN from '../../assets/img/TeamN.svg';
+import SearchComponent from './SearchComponent';
+const Newtab: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+  };
   return (
     <div
-      className="App"
       style={{
-        backgroundColor: theme === 'light' ? '#ffffff' : '#000000',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '50vh',
       }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' && '#0281dc', marginBottom: '10px' }}>
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
+      <div>
+        <img src={chart} alt="" style={{ width: '300px', height: '400px', margin: '50vh auto auto 57vw' }} />
+      </div>
+      {/* 첫번째 줄 프레임 */}
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <button
-          style={{
-            backgroundColor: theme === 'light' ? '#fff' : '#000',
-            color: theme === 'light' ? '#000' : '#fff',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
+          onClick={() => handleCategoryChange('health')}
+          className="margin: '8px', width: '700px', height: '700px'">
+          <img src={health} alt="health" />
         </button>
-        <span className="text-lime-400">The color of this paragraph is defined using Tailwind CSS.</span>
-      </header>
+        <img src={game} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={economy} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={science} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={edu} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+      </div>
+      {/* 두번째 줄 프레임 */}
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <img src={animal} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={social} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={sport} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={travel} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={enter} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+      </div>
+      {/* 세번째 줄 프레임 */}
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <img src={art} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={cook} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={music} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={smile} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+        <img src={stc} alt="" style={{ margin: '8px', width: '700px', height: '700px' }} />
+      </div>
+      <div>
+        <img src={TeamN} alt="" style={{ width: '500px', height: '200px', marginLeft: '3250px' }} />
+      </div>
+      <SearchComponent selectedCategory={selectedCategory} />
     </div>
   );
 };
-
 export default withErrorBoundary(withSuspense(Newtab, <div> Loading ... </div>), <div> Error Occur </div>);
