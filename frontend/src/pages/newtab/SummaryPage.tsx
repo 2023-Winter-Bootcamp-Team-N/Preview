9108dca7b3f20937b1df3629aed6aacd1886fa50
신수진 <sinsujin@sinsujin-ui-MacBookPro.local>
2024-01-09 22:58:16 +0900

    feat : 요약본 파트 되돌렸어요

--- frontend/src/pages/newtab/SummaryPage.tsx
+++ frontend/src/pages/newtab/SummaryPage.tsx
@@ -76,14 +76,14 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
         <div key={index} style={{ display: 'flex', flexDirection: 'column' }}>
           {/* 요약본 {index} */}
           {/* 라인 */}
-          <img src={line} alt={`Line ${index} Icon`} className="w-full h-[0.0625rem] my-2" />
+          <img src={line} alt={`Line ${index} Icon`} style={{ width: '90%', height: 'auto', margin: '5%' }} />
           {/* 썸네일, 텍스트*/}
           <div style={{ display: 'flex', flexDirection: 'row' }}>
             {/* 썸네일 */}
             <img
               src={youtubeimage}
               alt={`Thumbnail ${index} Icon`}
-              className="w-1/3 h-auto m-0 md:w-[15.49rem] md:h-[8.75rem] md:m-0 md:mr-6"
+              style={{ width: '33%', height: 'auto', margin: '0 5%' }}
             />
             {/* 텍스트 */}
             <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -92,42 +92,40 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
                 {/* 제목, 날짜를 한 행에 */}
                 {/* 제목 */}
                 <textarea
-                  className="text-black outline-none bg-transparent p-1 w-60 md:w-[14.5rem] h-[-2.4375rem] resize-none text-bold"
+                  className="text-black outline-none bg-transparent p-1 w-60 resize-none text-bold"
                   style={{
+                    fontSize: '26px',
                     fontWeight: '700',
                     lineHeight: 'normal',
                     fontFamily: 'notoSans',
                   }}
-                  value={'가나다라가나다라가나다라가나다라'}
-                  readOnly // 사용자 입력 비활성화
+                  value={'가나다라'}
+                  readOnly
                 />
                 {/* 날짜 */}
                 <textarea
-                  className="text-black font-['notoSans'] outline-none bg-transparent p-1 w-35 md:w-[8rem] h-[1.6875rem] resize-none"
+                  className="text-black font-['notoSans'] outline-none bg-transparent p-1 w-35 resize-none"
                   style={{
-                    fontWeight: '500',
+                    fontSize: '20px',
                     fontFamily: 'notoSans',
                   }}
                   value={'2024.01.08'}
                   readOnly
-                />{' '}
+                />
               </div>
-              <div
-                style={{
-                  marginRight: '30%',
-                }}>
+              <div className="mr-30">
                 {/* 요약본 */}
                 <textarea
-                  className="text-black outline-none bg-transparent p-1 w-full md:w-[21.625rem] h-[-6.5rem] resize-none"
+                  className="text-black outline-none bg-transparent p-1 w-full resize-none"
                   style={{
-                    fontWeight: '400',
+                    fontSize: '18px',
                     fontFamily: 'notoSans',
                   }}
                   rows={4}
                   value={
                     '대한민국의 경제질서는 개인과 기업의 경제상의 자유와 창의를 존중함을 기본으로 한다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다.'
                   }
-                  readOnly // 사용자 입력 비활성화
+                  readOnly
                 />
               </div>
             </div>
