c2404770923a73198d3de2e6da7ed944df1db6f8
sejongpark <sejong427@sungkyul.ac.kr>
2024-01-09 22:20:48 +0900

    feat: tailwind 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/**/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
     extend: {},
   },
