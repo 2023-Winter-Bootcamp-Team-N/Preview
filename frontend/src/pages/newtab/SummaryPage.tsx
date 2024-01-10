e3e32714f6ae10f33baf7eb245ebb955eba3b429
신수진 <sinsujin@sinsujin-ui-MacBookPro.local>
2024-01-11 04:55:45 +0900

    feat : 시간대별 이미지 파란이미지로 수정

--- frontend/src/pages/newtab/SummaryPage.tsx
+++ frontend/src/pages/newtab/SummaryPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import searchIcon from '../../assets/img/searchIcon.svg';
 import youtubeimage from '../../assets/img/youtubeimage.png';
 import line from '../../assets/img/line.svg';
+import sectionImage from '../../assets/img/sectionImage.svg';
 
 import './SummaryPage.css';
 
@@ -166,14 +167,81 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
               </div>
             </div>
           </div>
+          {/*안쪽 div : 요약본 확대하기 */}
           {selectedItem === index && (
-            <div
-              style={{
-                border: '1px solid red',
-                padding: '10px',
-                margin: '10px',
-              }}>
-              이것이 안쪽 div입니다!
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              {/* 썸네일 */}
+              <img
+                src={sectionImage}
+                alt={`Thumbnail ${index} Icon`}
+                style={{ width: '33%', height: '15%', marginLeft: '5%', marginRight: '5%' }}
+              />
+              {/* 텍스트 */}
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                {/* 제목, 날짜 */}
+                <div style={{ display: 'flex', flexDirection: 'row', height: '20%' }}>
+                  {/* 제목, 날짜를 한 행에 */}
+                  {/* 제목 */}
+                  <pre
+                    //className="text-black outline-none bg-transparent p-1 w-80 resize-none text-bold overflow-hidden"
+                    style={{
+                      color: 'black',
+                      outline: 'none',
+                      fontFamily: 'notoSans',
+                      background: 'transparent',
+                      width: '60%',
+                      resize: 'none',
+                      overflow: 'hidden',
+                      fontSize: '1.8vw',
+                      fontWeight: '700',
+                      lineHeight: 'normal',
+                      alignSelf: 'flex-start',
+                      verticalAlign: 'bottom',
+                    }}>
+                    안쪽안쪽 요약본요약본
+                  </pre>
+                  {/* 날짜 */}
+                  <pre
+                    style={{
+                      color: 'black',
+                      outline: 'none',
+                      background: 'transparent',
+                      width: '25%',
+                      resize: 'none',
+                      overflow: 'hidden',
+                      fontSize: '1.2vw',
+                      marginRight: '2%',
+                      marginTop: '2%',
+                      fontFamily: 'notoSans',
+                      whiteSpace: 'pre-wrap',
+                    }}>
+                    2024.01.08
+                  </pre>
+                </div>
+                <div className="mr-30">
+                  {/* 요약본 */}
+                  <pre
+                    style={{
+                      color: 'black',
+                      outline: 'none',
+                      background: 'transparent',
+                      width: '85%',
+                      resize: 'none',
+                      overflow: 'hidden',
+                      fontSize: '1.06vw',
+                      margin: '2% 5% 2% 0',
+                      marginRight: '2%',
+                      fontFamily: 'notoSans',
+                      alignSelf: 'flex-start',
+                      whiteSpace: 'pre-wrap',
+                      //maxHeight: '7.8rem',
+                    }}>
+                    2024 대한민국의 경제질서는 개인과 기업의 경제상의 자유와 창의를 존중함을 기본으로 한다. 국정감사 및
+                    조사에 관한 절차 기타 필요한 사항은 법률로 정한다. 대한민국의 경제질서는 개인과 기업의 경제상의
+                    자유와 창의를 존중함을 기본으로 한다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다.
+                  </pre>
+                </div>
+              </div>
             </div>
           )}
         </div>
