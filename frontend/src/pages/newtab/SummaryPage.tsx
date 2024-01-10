1b906a923b5028d8419c16ea11929af853fe875c
신수진 <sinsujin@sinsujin-ui-MacBookPro.local>
2024-01-10 13:44:05 +0900

    feat : 요약본 textarea 부분 테일윈드를 css로

--- frontend/src/pages/newtab/SummaryPage.tsx
+++ frontend/src/pages/newtab/SummaryPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import searchIcon from '../../assets/img/searchIcon.png';
-import youtubeimage from '../../assets/img/youtubeimage.svg';
+import youtubeimage from '../../assets/img/youtubeimage.png';
 import line from '../../assets/img/line.png';
 
 import './SummaryPage.css';
@@ -54,7 +54,7 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
 
             <img src={searchIcon} alt="Search Icon" style={{ height: 'auto', marginRight: '1vw', width: '1.5vw' }} />
             {/* 인풋 바 */}
-            <div style={{ background: '#F5F5F7', marginRight: '5rem' }}>
+            <div style={{ background: '#F5F5F7', marginRight: '4rem' }}>
               {' '}
               <input
                 style={{
@@ -73,17 +73,17 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
       </div>
 
       {[1, 2, 3, 4].map(index => (
-        <div key={index} style={{ display: 'flex', flexDirection: 'column' }}>
+        <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
           {/* 요약본 {index} */}
           {/* 라인 */}
-          <img src={line} alt={`Line ${index} Icon`} style={{ width: '90%', height: 'auto', margin: '5%' }} />
+          <img src={line} alt={`Line ${index} Icon`} style={{ width: '90%', height: 'auto', margin: '7% 5% 7% 5%' }} />
           {/* 썸네일, 텍스트*/}
           <div style={{ display: 'flex', flexDirection: 'row' }}>
             {/* 썸네일 */}
             <img
               src={youtubeimage}
               alt={`Thumbnail ${index} Icon`}
-              style={{ width: '33%', height: 'auto', margin: '0 5%' }}
+              style={{ width: '33%', height: 'auto', marginLeft: '5%', marginRight: '5%', alignSelf: 'flex-start' }}
             />
             {/* 텍스트 */}
             <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -92,22 +92,27 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
                 {/* 제목, 날짜를 한 행에 */}
                 {/* 제목 */}
                 <textarea
-                  className="text-black outline-none bg-transparent p-1 w-60 resize-none text-bold overflow-hidden"
+                  className="text-black outline-none bg-transparent p-1 w-80 resize-none text-bold overflow-hidden"
                   style={{
-                    fontSize: '26px',
+                    fontSize: '1.8vw', // 화면 너비의 2%로 글씨 크기 지정
                     fontWeight: '700',
                     lineHeight: 'normal',
                     fontFamily: 'notoSans',
+                    alignSelf: 'flex-start',
+                    verticalAlign: 'bottom',
                   }}
                   value={'가나다라'}
                   readOnly
                 />
                 {/* 날짜 */}
                 <textarea
-                  className="text-black font-['notoSans'] outline-none bg-transparent p-1 w-35 resize-none overflow-hidden"
+                  className="text-black font-['notoSans'] outline-none bg-transparent p-1 w-15 resize-none overflow-hidden"
                   style={{
-                    fontSize: '20px',
-                    fontFamily: 'notoSans',
+                    fontSize: '1.2vw', // 화면 너비의 2%로 글씨 크기 지정
+                    alignSelf: 'flex-start',
+                    marginRight: '2%', // 오른쪽 마진 추가
+
+                    whiteSpace: 'pre-wrap', //텍스트가 화면을 넘어가는 경우, 해당 텍스트에 줄 바꿈을 추가
                   }}
                   value={'2024.01.08'}
                   readOnly
@@ -116,12 +121,23 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
               <div className="mr-30">
                 {/* 요약본 */}
                 <textarea
-                  className="text-black outline-none bg-transparent p-1 w-full resize-none overflow-hidden"
+                  //className="text-black outline-none bg-transparent p-1 w-80 resize-none overflow-hidden"
                   style={{
-                    fontSize: '18px',
-                    fontFamily: 'notoSans',
+                    color: 'black',
+                    outline: 'none',
+                    background: 'transparent',
+                    padding: '1px', // 조절이 필요한 경우에는 원하는 크기로 조절
+                    width: '80%', // 원하는 가로 크기
+                    resize: 'none', // 크기 조절 비활성화
+                    overflow: 'auto', // 스크롤 막기
+                    fontSize: '1vw', // 화면 너비의 2%로 글씨 크기 지정
+                    margin: '2% 5% 2% 0',
+                    marginRight: '2%', // 오른쪽 마진 추가
+                    alignSelf: 'flex-start',
+                    whiteSpace: 'pre-wrap',
+                    maxHeight: '3rem',
                   }}
-                  rows={4}
+                  //rows={4}
                   value={
                     '대한민국의 경제질서는 개인과 기업의 경제상의 자유와 창의를 존중함을 기본으로 한다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다.'
                   }
