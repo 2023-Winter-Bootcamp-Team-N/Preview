ee4b92aad47c47d04cb7dca9df7b36cd526b0ba0
박세종 <sejong.park.2000@gmail.com>
2024-01-11 17:13:03 +0900

    feat: 버튼 클릭 시 클립보드 복사, Newtab 이동

--- frontend/src/pages/sidepanel/SidePanel.tsx
+++ frontend/src/pages/sidepanel/SidePanel.tsx
@@ -1,13 +1,27 @@
-import '@pages/sidepanel/SidePanel.css';
-import withSuspense from '@src/shared/hoc/withSuspense';
-import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
+import React from 'react';
 import save from '@assets/img/savebutton.svg';
 import mypage from '@assets/img/mypagebutton.svg';
 import copy from '@assets/img/copybutton.svg';
 import teamlogo from '@assets/img/teamlogo.svg';
-import React from 'react';
+import '@pages/sidepanel/SidePanel.css';
+import withSuspense from '@src/shared/hoc/withSuspense';
+import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 
 const SidePanel = () => {
+  // 텍스트 복사 함수
+  const copyText = () => {
+    const text = document.querySelector('.side-panel p').textContent;
+    navigator.clipboard
+      .writeText(text)
+      .then(() => console.log('텍스트가 복사 되었습니다.'))
+      .catch(err => console.error('텍스트 복사 실패: ', err));
+  };
+
+  // 새 탭 열기 함수
+  const openNewTab = () => {
+    chrome.tabs.create({ url: 'chrome://newtab' });
+  };
+
   return (
     <div className="rounded-lg bg-color p-4 space-y-4 border-none side-panel">
       {/* 상단 로고, 서비스명, 버튼 영역 */}
@@ -17,14 +31,14 @@ const SidePanel = () => {
           <span className="font-semibold text-xl">요약 서비스</span>
         </div>
         <div className="flex -space-x-1">
-          <button className="p-2 rounded">
-            <img src={save} alt="logo" className="w-5 h-5 " />
+          <button className="save-button p-2 rounded">
+            <img src={save} alt="save logo" className="w-5 h-5" />
           </button>
-          <button className="p-2 rounded">
-            <img src={mypage} alt="logo" className="w-5 h-5 " />
+          <button className="mypage-button p-2 rounded" onClick={openNewTab}>
+            <img src={mypage} alt="mypage logo" className="w-5 h-5" />
           </button>
-          <button className="p-2 rounded">
-            <img src={copy} alt="logo" className="w-5 h-5" />
+          <button className="copy-button p-2 rounded" onClick={copyText}>
+            <img src={copy} alt="copy logo" className="w-5 h-5" />
           </button>
         </div>
       </div>
@@ -37,4 +51,4 @@ const SidePanel = () => {
   );
 };
 
-export default withErrorBoundary(withSuspense(SidePanel, <div> Loading ... </div>), <div> Error Occur </div>);
+export default withErrorBoundary(withSuspense(SidePanel, <div>Loading...</div>), <div>Error Occur</div>);
