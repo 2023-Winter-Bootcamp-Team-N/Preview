35a8d0b8bfeac4699c0259c7ce16d796ab55454f
신수진 <sinsujin@sinsujin-ui-MacBookPro.local>
2024-01-11 22:53:27 +0900

    feat : timeSummary-container 오른쪽에 가져지게 구현

--- frontend/src/pages/newtab/SummaryPage.tsx
+++ frontend/src/pages/newtab/SummaryPage.tsx
@@ -13,11 +13,14 @@ interface SummaryPageProps {
 
 const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
   const [selectedItem, setSelectedItem] = useState<number | null>(null);
+  const [istimeSummaryVisible, setIstimeSummaryVisible] = useState(false); // 새로운 상태 추가
 
   const toggleInnerDiv = (index: number) => {
     setSelectedItem(prev => (prev === index ? null : index));
+    setIstimeSummaryVisible(false); // 다른 항목 클릭 시 timeSummary 감추기
   };
 
+  //카테고리를 선택하면 요약본이 보여지는 함수
   const [isSummaryVisible, setIsSummaryVisible] = useState(false);
   useEffect(() => {
     setIsSummaryVisible(!!selectedCategory);
@@ -28,6 +31,10 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
     setIsSummaryVisible(false); // 창이 닫히도록 상태를 변경
   };
 
+  // timeSummary를 토글하는 함수
+  const toggleTimeSummary = () => {
+    setIstimeSummaryVisible(prev => !prev);
+  };
   return (
     <div
       className={`summary-container ${isSummaryVisible ? 'visible' : ''}`}
@@ -171,89 +178,92 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
               </div>
             </div>
           </div>
-          {/*안쪽 div : 요약본 확대하기 */}
+          {/* 여기에 안쪽 div 넣었었삼 */}
           {selectedItem === index && (
             <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                backgroundColor: '#D9D9D9',
-                borderRadius: '2%',
-              }}>
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                {/* <시간대별요약> 텍스트 */}
-                <pre
-                  style={{
-                    backgroundColor: '#D9D9D9',
-                    height: '5rem',
-                    width: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    alignSelf: 'flex-start',
-                    fontSize: '1.4vw',
-                    fontFamily: 'notoSans',
-                    fontWeight: '400',
-                    paddingLeft: '5%', // 왼쪽 패딩 추가\
-                    borderRadius: '5%',
-                  }}>
-                  ...시간대별 요약...
-                </pre>
-                <img
-                  src={sectiondevider}
-                  alt={`Line ${index} Icon`}
-                  style={{ width: '90%', height: 'auto', margin: '0 5% 0 5%' }}
-                />
-              </div>
-              {[1, 2, 3, 4].map(index => (
-                // 각 섹션들은 열 기준으로 나열
-                <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                    {/* 썸네일과 요약본을 한 행에 */}
-                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                      {/* 썸네일 */}
-                      <img
-                        src={sectionImage}
-                        alt={`Thumbnail ${index} Icon`}
-                        style={{
-                          width: '33%',
-                          height: '15%',
-                          marginLeft: '5%',
-                          marginRight: '5%',
-                          alignSelf: 'center',
-                        }}
-                      />
-                      <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        {/* 시간버튼과 요약본을 한 열에 */}
-                        {/* 시간버튼 */}
-                        <button></button>
-                        {/* 요약본 */}
-                        <pre
+              className={`timeSummary-container ${istimeSummaryVisible ? 'visible' : ''}`}
+              style={{ border: '1px solid #8D8D8D', overflow: 'auto' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  backgroundColor: '#D9D9D9',
+                  borderRadius: '2%',
+                }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                  {/* <시간대별요약> 텍스트 */}
+                  <pre
+                    style={{
+                      backgroundColor: '#D9D9D9',
+                      height: '5rem',
+                      width: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      alignSelf: 'flex-start',
+                      fontSize: '1.4vw',
+                      fontFamily: 'notoSans',
+                      fontWeight: '400',
+                      paddingLeft: '5%', // 왼쪽 패딩 추가\
+                      borderRadius: '5%',
+                    }}>
+                    ...시간대별 요약...
+                  </pre>
+                  <img
+                    src={sectiondevider}
+                    alt={`Line ${index} Icon`}
+                    style={{ width: '90%', height: 'auto', margin: '0 5% 0 5%' }}
+                  />
+                </div>
+                {[1, 2, 3, 4].map(index => (
+                  // 각 섹션들은 열 기준으로 나열
+                  <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                      {/* 썸네일과 요약본을 한 행에 */}
+                      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        {/* 썸네일 */}
+                        <img
+                          src={sectionImage}
+                          alt={`Thumbnail ${index} Icon`}
                           style={{
-                            color: 'black',
-                            alignSelf: 'flex-start', // 수정된 부분
+                            width: '33%',
+                            height: '15%',
+                            marginLeft: '5%',
+                            marginRight: '5%',
+                            alignSelf: 'center',
+                          }}
+                        />
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                          {/* 시간버튼과 요약본을 한 열에 */}
+                          {/* 시간버튼 */}
+                          <button></button>
+                          {/* 요약본 */}
+                          <pre
+                            style={{
+                              color: 'black',
+                              alignSelf: 'flex-start', // 수정된 부분
 
-                            outline: 'none',
-                            background: 'transparent',
-                            width: '85%',
-                            resize: 'none',
-                            overflow: 'hidden',
-                            fontSize: '1.06vw',
-                            margin: '2% 5% 2% 0',
-                            marginRight: '2%',
-                            fontFamily: 'notoSans',
-                            whiteSpace: 'pre-wrap',
-                            //maxHeight: '7.8rem',
-                          }}>
-                          - 자기 사운드가 고장났다고 생각하는 경우, 화면도 꺼진 상태라면 사고로 간주될 수 있음 - 온라인
-                          강좌에서는 움직이는 그래픽 디자인을 심플하게 하는 것이 시선 집중에 유리함 - 디자인에 요소를
-                          많이 넣는 것보다, 깔끔하고 심플한 디자인이 더 좋은 결과물을 얻을 수 있음 - 배경음악과 효과음은
-                          영상의 분위기를 크게 바꿀 수 있으므로 중요한 역할을 함
-                        </pre>
+                              outline: 'none',
+                              background: 'transparent',
+                              width: '85%',
+                              resize: 'none',
+                              overflow: 'hidden',
+                              fontSize: '1.06vw',
+                              margin: '2% 5% 2% 0',
+                              marginRight: '2%',
+                              fontFamily: 'notoSans',
+                              whiteSpace: 'pre-wrap',
+                              //maxHeight: '7.8rem',
+                            }}>
+                            - 자기 사운드가 고장났다고 생각하는 경우, 화면도 꺼진 상태라면 사고로 간주될 수 있음 -
+                            온라인 강좌에서는 움직이는 그래픽 디자인을 심플하게 하는 것이 시선 집중에 유리함 - 디자인에
+                            요소를 많이 넣는 것보다, 깔끔하고 심플한 디자인이 더 좋은 결과물을 얻을 수 있음 - 배경음악과
+                            효과음은 영상의 분위기를 크게 바꿀 수 있으므로 중요한 역할을 함
+                          </pre>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  {/* 라인 이미지
+                    {/* 라인 이미지
                   <img
                     src={sectionImage}
                     alt={`Thumbnail ${index} Icon`}
@@ -265,8 +275,9 @@ const SummaryPage: React.FC<SummaryPageProps> = ({ selectedCategory }) => {
                       alignSelf: 'center',
                     }}
                   /> */}
-                </div>
-              ))}
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </div>
