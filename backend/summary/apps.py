49bf71f66890c5e16e0aa29dbec457e4adaaddb6
김주언 <wndjs803@naver.com>
2024-01-14 04:21:51 +0900

    fix: 오류 수정

--- backend/summary/apps.py
+++ backend/summary/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
-from summary.cron import update_summary
-
 
 class SummaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'summary'
 
     def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from apscheduler.triggers.cron import CronTrigger
+        from summary.cron import update_summary
         # 스케줄러 초기화
         sched = BackgroundScheduler()
         # Django 앱이 시작될 때 스케줄러 시작
