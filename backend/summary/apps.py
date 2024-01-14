9b20b65d06a4f832b4ee6b4ba9c6078e55ca1548
suha <cksun080@gmail.com>
2024-01-14 20:21:24 +0900

    feat: 요약 자동화

--- backend/summary/apps.py
+++ backend/summary/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
 
-
-class AccountConfig(AppConfig):
+class SummaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+    name = 'summary'
+
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from apscheduler.triggers.interval import IntervalTrigger
+        from summary.cron import update_summary
+        # 스케줄러 초기화
+        sched = BackgroundScheduler()
+        # Django 앱이 시작될 때 스케줄러 시작
+        sched.start()
+
+        # 스케줄러에 작업 추가
+        sched.add_job(
+            update_summary,
+            trigger=IntervalTrigger(seconds=60),
+            id='daily_job',
+            name='60초마다 작업',
+            max_instances=1,
+            replace_existing=True,
+)
---
+++ backend/summary/cron.py
@@ -0,0 +1,44 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from django.utils import timezone
+from summary.models import Summary
+from subscribe.models import Subscribe
+
+# YouTube API 관련 설정
+API_KEY = ''  # 본인의 YouTube API 키로 대체
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def update_summary():
+    # 매일 00:00에 실행되는 작업
+    now = timezone.now()
+
+    # 모든 사용자들의 구독 채널을 불러오기
+    subscriptions = Subscribe.objects.distinct('subscribe_channel').select_related('user_id')
+
+    for subscribe in subscriptions:
+        user_id = subscribe.user_id
+        subscribe_channel = subscribe.subscribe_channel
+
+        # 구독 채널에 새로운 영상이 올라왔는지 확인
+        try:
+            youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=API_KEY)
+            response = youtube.search().list(
+                q=f"{subscribe_channel}",
+                type='video',
+                part='id',
+                order='date',
+                maxResults=1
+            ).execute()
+
+            for item in response.get('items', []):
+                video_id = item['id']['videoId']
+                # 새로운 영상이 확인 되었다면 영상의 URL을 출력
+                video_url = f'https://www.youtube.com/watch?v={video_id}'
+                print(f"새로운 영상 발견! 사용자 ID: {user_id}, 채널: {subscribe_channel}, URL: {video_url}")
+
+                summary = Summary.objects.create(user_id=user_id, youtube_url=video_url)
+                summary.save()
+
+        except HttpError as e:
+                print(f'YouTube API 오류: {e}')
