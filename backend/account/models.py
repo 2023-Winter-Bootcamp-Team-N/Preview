64dfaa886ca98e75c090c9ad686c49da8d9aa9dc
김주언 <wndjs803@naver.com>
2024-01-18 21:28:32 +0900

    fix: deploy.yml 수정

--- backend/account/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    email = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=20, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True)
