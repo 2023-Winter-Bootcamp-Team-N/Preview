65a32bbd7cb240c773a75a84da31e04e0c990aef
김주언 <wndjs803@naver.com>
2024-01-19 01:41:52 +0900

    fix: docker-compose.yml 수정

--- backend/subscribe/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SubscribeAPIView, SubscribeCancelAPIView
-
-urlpatterns = [
-    path('', SubscribeAPIView.as_view()),
-    path('<str:subscribe_channel>', SubscribeCancelAPIView.as_view()),
-]
