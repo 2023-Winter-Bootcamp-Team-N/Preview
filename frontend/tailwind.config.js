0372f429e42788488ea09841753a728179e24733
신수진 <tnwls5621@tukorea.ac.kr>
2024-01-20 03:58:14 +0900

    feat : wantedSans 굵기별 폰트 설치

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     extend: {
       fontFamily: {
         WantedSansRegular: ['WantedSansRegular', 'sans-serif'],
+        WantedSansBlack: ['WantedSansRegular', 'sans-serif'],
+        WantedSansBold: ['WantedSansRegular', 'sans-serif'],
+        WantedSansExtraBlack: ['WantedSansRegular', 'sans-serif'],
+        WantedSansExtraBold: ['WantedSansRegular', 'sans-serif'],
+        WantedSansMedium: ['WantedSansRegular', 'sans-serif'],
+        WantedSansSemiBold: ['WantedSansRegular', 'sans-serif'],
       },
     },
   },
