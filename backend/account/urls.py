951085561be5beb43d825508dfef606a2520c29a
김주언 <wndjs803@naver.com>
2024-01-19 16:18:44 +0900

    test: 자동 요약 확인 중

--- backend/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import MembersAPIView, LoginAPIView
-
-
-urlpatterns = [
-    path('signin', MembersAPIView.as_view()),
-    path('login', LoginAPIView.as_view()),
-]
-
