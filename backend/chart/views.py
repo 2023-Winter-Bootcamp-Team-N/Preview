9e0d9475070178e5003825b63f82dce596ee3838
김주언 <wndjs803@naver.com>
2024-01-23 02:54:53 +0900

    feat: 구독 목록 api

---
+++ backend/chart/views.py
@@ -0,0 +1,64 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from summary.models import Summary, Category
+from summary.serializers import UserIdParameterSerializer
+from .serializers import CategoryChartResponseSerializer, SubscribeChartResponseSerializer
+from django.db.models import Count
+
+from drf_yasg.utils import swagger_auto_schema
+
+class CategoryChartAPIView(APIView):
+    @swagger_auto_schema(operation_summary="카테고리 차트", query_serializer=UserIdParameterSerializer, responses={"200":CategoryChartResponseSerializer})
+    def get(self, request):
+        user_id = request.query_params.get('user_id', None)
+        if not user_id:
+            return Response({'error': '유저가 존재하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user_summaries = Summary.objects.filter(user_id=user_id, deleted_at__isnull=True)
+
+        category_counts = Category.objects.filter(summary_id__in=user_summaries).values('category').annotate(count=Count('summary_id'))
+
+        if not category_counts.exists():
+            return Response({"error": "해당 정보가 없습니다"}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = {
+            'categories': [{'category': item['category'], 'count': item['count']} for item in category_counts]
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
+    
+
+class SubscribeChartAPIView(APIView):
+    @swagger_auto_schema(operation_summary="채널 차트", query_serializer=UserIdParameterSerializer, responses={"200":SubscribeChartResponseSerializer})
+    def get(self, request):
+        user_id = request.query_params.get('user_id', None)
+        if not user_id:
+            return Response({'error': '유저가 존재하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 유저의 각 구독 채널 정보 가져오기
+        user_subscribes = Summary.objects.filter(user_id=user_id).values('youtube_channel').distinct()
+
+        channels = []
+        for subscribe in user_subscribes:
+            channel_name = subscribe['youtube_channel']
+            summary_count = Summary.objects.filter(user_id=user_id, youtube_channel=channel_name).count()
+
+            channels.append({
+                'youtube_channel': channel_name,
+                'count': summary_count,
+            })
+
+        if not channels:
+            response = {
+                "error": "해당되는 정보가 없습니다.",
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        
+        # API 응답 데이터 생성
+        data = {
+            'subscribes': channels
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
---
+++ frontend/src/pages/newtab/SubscribePage.tsx
@@ -0,0 +1,143 @@
+import { useState } from 'react';
+import channelBg from '../../assets/img/channelBg.svg';
+import YoutubeChannelProfile from '../../assets/img/YoutubeChannelProfile.svg';
+import YoutubeChannelProfile2 from '../../assets/img/YoutubeChannelProfile2.svg';
+import YoutubeChannelProfile3 from '../../assets/img/YoutubeChannelProfile3.svg';
+import YoutubeChannelProfile4 from '../../assets/img/YoutubeChannelProfile4.svg';
+import YoutubeChannelProfile5 from '../../assets/img/YoutubeChannelProfile5.svg';
+import YoutubeChannelProfile6 from '../../assets/img/YoutubeChannelProfile6.svg';
+import YoutubeChannelProfile7 from '../../assets/img/YoutubeChannelProfile7.svg';
+import YoutubeChannelProfile8 from '../../assets/img/YoutubeChannelProfile8.svg';
+import SubscribeText from '../../assets/img/SubscribeText.svg';
+import SubscribeModal from './SubscribeModal';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, react/prop-types
+const SubscribePage = ({ user_id, selectedChannel, setSelectedChannel }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // 이미지 클릭 핸들러
+  const handleImageClick = channel => {
+    setSelectedChannel(channel);
+    setIsModalOpen(true);
+  };
+  const handleChannelSubmit = channelName => {
+    console.log('Submitted Channel: ', channelName);
+    // 필요한 로직 추가 (예: API 호출)
+  };
+  console.log('Rendering ChartComponent');
+
+  return (
+    <div>
+      <div className={`main-content ${selectedChannel ? 'search-visible' : ''}`} style={{ position: 'relative' }}>
+        <div className={`main-content`} style={{ position: 'relative' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>
+              {/* 제목 추가 */}
+              <img src={SubscribeText} alt="SubscribeText" style={{ marginLeft: '1.5rem', width: '250px' }} />
+
+              <div style={{ position: 'relative', width: '800px', height: '480px' }}>
+                <img src={channelBg} alt="Channel Background" style={{ width: '100%', height: '100%' }} />
+
+                {/* 윗줄 이미지 */}
+
+                <button
+                  onClick={() => handleImageClick('Channel1')}
+                  style={{ position: 'absolute', top: '130px', left: '140px' }}>
+                  <img
+                    src={YoutubeChannelProfile}
+                    alt="Youtube Channel Profile1"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 두 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel2')}
+                  style={{ position: 'absolute', top: '130px', left: '280px' }}>
+                  <img
+                    src={YoutubeChannelProfile2}
+                    alt="Youtube Channel Profile 2"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 세 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel3')}
+                  style={{ position: 'absolute', top: '130px', left: '420px' }}>
+                  <img
+                    src={YoutubeChannelProfile3}
+                    alt="Youtube Channel Profile 3"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 네 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel4')}
+                  style={{ position: 'absolute', top: '130px', left: '560px' }}>
+                  <img
+                    src={YoutubeChannelProfile4}
+                    alt="Youtube Channel Profile 4"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 다섯 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel5')}
+                  style={{ position: 'absolute', top: '250px', left: '140px' }}>
+                  <img
+                    src={YoutubeChannelProfile5}
+                    alt="Youtube Channel Profile 5"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 여섯 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel6')}
+                  style={{ position: 'absolute', top: '250px', left: '280px' }}>
+                  <img
+                    src={YoutubeChannelProfile6}
+                    alt="Youtube Channel Profile 6"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 일곱 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel7')}
+                  style={{ position: 'absolute', top: '250px', left: '420px' }}>
+                  <img
+                    src={YoutubeChannelProfile7}
+                    alt="Youtube Channel Profile 7"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+
+                {/* 여덟 번째 이미지 버튼 */}
+                <button
+                  onClick={() => handleImageClick('Channel8')}
+                  style={{ position: 'absolute', top: '250px', left: '560px' }}>
+                  <img
+                    src={YoutubeChannelProfile8}
+                    alt="Youtube Channel Profile 8"
+                    style={{ width: '100px', height: '100px', borderRadius: '30px' }}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <SubscribeModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onChannelSubmit={handleChannelSubmit}
+      />
+    </div>
+  );
+};
+
+export default SubscribePage;
