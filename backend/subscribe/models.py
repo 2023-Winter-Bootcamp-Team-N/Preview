5a5b941da6cb2f6a1cd67e4748107f77e5ff1b41
김주언 <wndjs803@naver.com>
2024-01-23 17:08:11 +0900

    fix: subscribe models.py 수정

--- backend/subscribe/models.py
+++ backend/subscribe/models.py
@@ -4,7 +4,8 @@ from account.models import User
 
 class Subscribe(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    subscribe_channel = models.CharField(max_length=100, null=False)
+    subscribe_channel_id = models.CharField(max_length=100, null=False)
+    subscribe_channel_name = models.CharField(max_length=100, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
