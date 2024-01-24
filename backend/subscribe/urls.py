140189200a11ff4cd3b54eb5a8c1a4d665a43a1c
김주언 <wndjs803@naver.com>
2024-01-24 15:32:23 +0900

    fix: subscribe cancle api 수정

--- backend/subscribe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SubscribeAPIView, SubscribeCancelAPIView, SubscribeListAPIView
-
-urlpatterns = [
-    path('', SubscribeAPIView.as_view()),
-    path('<str:subscribe_channel_id>', SubscribeCancelAPIView.as_view()),
-    path('list/', SubscribeListAPIView.as_view())
-]
