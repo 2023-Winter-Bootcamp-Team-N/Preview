9bd94ddeb371e9a31750366d030d10b9cf87751d
신수진 <tnwls5621@tukorea.ac.kr>
2024-01-29 16:01:33 +0900

    design : 모달창 뒷 배경 밝기 감소

--- frontend/src/pages/newtab/Modal.tsx
+++ frontend/src/pages/newtab/Modal.tsx
@@ -85,7 +85,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, closeModal, selectedSummary, onDe
           left: 0,
           right: 0,
           bottom: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
           zIndex: 999,
           display: 'flex',
           justifyContent: 'center',
